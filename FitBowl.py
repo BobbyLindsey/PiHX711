c58f06051d230d6aa037567001a52ac052947a15
bobbylindsey <Bobby.Lindsey@gmail.com>
2017-06-07 17:34:33 -0400

    testing

---
+++ FitBowl.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+from hx7113 import HX711
+
+
+def cleanandexit():
+    print("Cleaning...")
+    GPIO.cleanup()
+    print("Bye!")
+    sys.exit()
+
+hx = HX711(5, 6)
+
+hx.set_reading_format("LSB", "MSB")
+
+hx.set_reference_unit(92)
+
+hx.reset()
+hx.tare()
+
+while True:
+    try:
+        val = hx.get_weight(1)
+        print(val)
+
+        hx.power_down()
+        hx.power_up()
+        time.sleep(0)
+    except (KeyboardInterrupt, SystemExit):
+        cleanandexit()
+
