afd58afe49ade38b53fc252eb213a816f63b1adc
bobbylindsey <Bobby.Lindsey@gmail.com>
2017-06-07 16:57:23 -0400

    testing

--- hx7113.py
+++ hx7113.py
@@ -71,8 +71,8 @@ class HX711:
         databits = [self.empty_bool_list[:]] * 3
         databytes = [0x0] * 4
 
-        for j in range(2, -1, -1):
-            for i in range(0, 8, 1):
+        for j in range(self.byte_range_values[0], self.byte_range_values[1], self.byte_range_values[2]):
+            for i in range(self.bit_range_values[0], self.bit_range_values[1], self.bit_range_values[2]):
                 GPIO.output(self.PD_SCK, True)  # Tell the HX711 to give you the next bit
                 time.sleep(0.000025)  # Wait 25 microseconds (wait time min 0.2 microsec and max 50 microsec)
                 GPIO.output(self.PD_SCK, False)  # Reset the read flag (doesn't clear the value to be read)
