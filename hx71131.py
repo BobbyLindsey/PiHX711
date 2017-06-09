40717b2ef5b2a56d77b2f21f536c187be4de4a42
bobbylindsey <Bobby.Lindsey@gmail.com>
2017-06-09 12:22:25 -0400

    testing

--- hx71131.py
+++ hx71131.py
@@ -65,8 +65,9 @@ class HX711:
                 # dataBits[j][i] = GPIO.input(self.DOUT)
                 # GPIO.output(self.PD_SCK, False)
                 GPIO.output(self.PD_SCK, True)  # Tell the HX711 to give you the next bit
-                time.sleep(0.00001)  # Wait 25 microseconds (wait time min 0.2 microsec and max 50 microsec)
+                time.sleep(0.000025)  # Wait 25 microseconds (wait time min 0.2 microsec and max 50 microsec)
                 GPIO.output(self.PD_SCK, False)  # Reset the read flag (doesn't clear the value to be read)
+                time.sleep(0.000001)  # Wait 1 microsecond as min low time for read flag (wiat time min 0.1 microsec)
                 dataBits[j][i] = GPIO.input(self.DOUT)  # Read the next bit into the array
             print(dataBits)
             dataBytes[j] = numpy.packbits(numpy.uint8(dataBits[j]))
