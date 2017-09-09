1f0e79723adf1fb1255e33a83c0229d3e7284444
Bobby Lindsey <Bobby.Lindsey@gmail.com>
2017-09-09 13:48:31 -0400

    Update example.py

--- example.py
+++ example.py
@@ -37,15 +37,15 @@ while True:
         # These three lines are useful to debug whether to use MSB or LSB in the reading formats
         # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
         # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment the three lines to see what it prints.
-        #np_arr8_string = hx.get_np_arr8_string()
-        #binary_string = hx.get_binary_string()
-        #print binary_string + " " + np_arr8_string
+        np_arr8_string = hx.get_np_arr8_string()
+        binary_string = hx.get_binary_string()
+        print binary_string + " " + np_arr8_string
         
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val = hx.get_weight(1)
-        print(val)
-        val = hx.get_value(1)
-        print(val)
+        #val = hx.get_weight(1)
+        #print(val)
+        #val = hx.get_value(1)
+        #print(val)
 
         hx.power_down()
         hx.power_up()
